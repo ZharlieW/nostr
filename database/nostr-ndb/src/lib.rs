598ebdc9a480c5bef1b62f3d9873220192c01767
ZharlieW <zhw306498704@gmail.com>
2026-01-19 12:14:36 +0800

    Update lib.rs

--- database/nostr-ndb/src/lib.rs
+++ database/nostr-ndb/src/lib.rs
@@ -23,7 +23,7 @@ use nostrdb::{
     Config, Filter as NdbFilter, IngestMetadata, Ndb, NdbStrVariant, Note, QueryResult, Transaction,
 };
 
-const MAX_RESULTS: i32 = 10_000;
+const MAX_RESULTS: i32 = i32::MAX;
 
 // Wrap `Ndb` into `NdbDatabase` because only traits defined in the current crate can be implemented for types defined outside the crate!
 
